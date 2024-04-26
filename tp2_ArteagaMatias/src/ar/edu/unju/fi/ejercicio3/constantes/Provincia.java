9a5c0295b98173900b3d1ce9ea1f73b3323bb78e
Matias <Matias@DESKTOP-5683VC2>
2024-04-25 23:30:37 -0300

    Creacion de el enum Provincia y la clase Main con el metodo main.

---
+++ tp2_ArteagaMatias/src/ar/edu/unju/fi/ejercicio3/constantes/Provincia.java
@@ -0,0 +1,23 @@
+package ar.edu.unju.fi.ejercicio3.constantes;
+
+public enum Provincia {
+	JUJUY(811611,53219), SALTA(1441351,155488), TUCUMAN(1687305,22525), CATAMARCA(429562,102602), LA_RIOJA(383865,89680), SANTIAGO_DEL_ESTERO(896461,136351);
+	
+	
+	private Integer CANTIDAD_POBLACION;
+	private Integer SUPERFICIE;
+	
+	private Provincia(Integer cANTIDAD_POBLACION, Integer sUPERFICIE) {
+		CANTIDAD_POBLACION = cANTIDAD_POBLACION;
+		SUPERFICIE = sUPERFICIE;
+	}
+
+	public Integer getCANTIDAD_POBLACION() {
+		return CANTIDAD_POBLACION;
+	}
+
+	public Integer getSUPERFICIE() {
+		return SUPERFICIE;
+	}
+	
+}
